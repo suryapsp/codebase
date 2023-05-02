e02ebd0c1d4a39b1eea19a112de7d32cc54368b8
suryapsp <techie-human@protonmail.com>
2023-05-02 19:59:27 +0530

    1b

---
+++ racket/basic/1a/test.rkt
@@ -0,0 +1,17 @@
+;; The first three lines of this file were inserted by DrRacket. They record metadata
+;; about the language level of this file in a form that our tools can easily process.
+#reader(lib "htdp-beginner-reader.ss" "lang")((modname test) (read-case-sensitive #t) (teachpacks ()) (htdp-settings #(#t constructor repeating-decimal #f #t none #f () #f)))
+;(sqrt 2)
+
+;"op1"
+;"op2"
+
+;(string-append "Bob" " " "Marley")
+
+;(string-length "surya")
+
+;            0123456
+;(substring "pratap" 3 6)
+
+;(substring "0123456" 3 6)
+
